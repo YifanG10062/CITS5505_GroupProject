4db8850bda28fa4246b012ade1b38747d5f0c4b1
Pavan23866945 <pavan23866945@gmail.com>
2025-05-05 21:18:55 +0800

    Update run.py
    removed duplicates

--- run.py
+++ run.py
@@ -5,10 +5,4 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-# run.py
-from app import create_app
 
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True)
