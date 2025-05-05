48154e8f601d0c1f27a55f83cd753da9d4509830
Yifan Gao <gyf971333964@gmail.com>
2025-05-05 23:30:15 +0800

    delete duplicate pages

--- Check/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('https://api.marketstack.com/v2/eod?access_key=4ece27fbd41ca25cb97f22cbf8713369&symbols=AAPL')
-    .then(response => response.json())
-    .then(data => {
-        const imageUrl = data.imageUrl; // Assuming the API returns an image URL
-        document.querySelector('.login-image').style.backgroundImage = `url(${imageUrl})`;
-    })
-    .catch(error => console.error('Failed to load image:', error));
-});
