afe8384d9a31a3d98460c5e4d0de97daed79a867
Yifan Gao <gyf971333964@gmail.com>
2025-05-06 01:23:30 +0800

    Minor Enhancement

--- app/__init__.py
+++ app/__init__.py
@@ -109,9 +109,9 @@ def create_app(config_class=DeploymentConfig):
                                  render_kw={"placeholder": "Password"})
 
     class RegistrationForm(FlaskForm):
-        FirstName = StringField(validators=[InputRequired(), Length(min=4, max=200)],
+        FirstName = StringField(validators=[InputRequired(), Length(min=2, max=200)],
                                 render_kw={"placeholder": "First Name"})
-        LastName = StringField(validators=[InputRequired(), Length(min=4, max=200)],
+        LastName = StringField(validators=[InputRequired(), Length(min=2, max=200)],
                                render_kw={"placeholder": "Last Name"})
         Email = StringField(validators=[InputRequired(), Email(), Length(min=4, max=200)],
                             render_kw={"placeholder": "Email"})
