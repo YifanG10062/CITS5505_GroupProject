dd494db2fbb7898453e311af5c83d82f53f36595
aolilfn <aoli.lfn@gmail.com>
2025-05-06 17:50:19 +0800

    Refactor: re-add password reset logic, email to fix later

--- app/forms/user.py
+++ app/forms/user.py
@@ -1,23 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Email, Length
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Email, Length, ValidationError
 
 class LoginForm(FlaskForm):
-    Email = StringField(validators=[InputRequired(), Email(), Length(min=4, max=200)],
-                        render_kw={"placeholder": "Email"})
-    Password = PasswordField(validators=[InputRequired(), Length(min=4, max=200)],
-                             render_kw={"placeholder": "Password"})
+    Email = StringField(validators=[InputRequired(), Email(message='Please enter valid email address.'), Length(min=1, max=200)], render_kw={"placeholder": "Email"})
+    Password = PasswordField(validators=[InputRequired(), Length(min=1, max=200)], render_kw={"placeholder": "Password"})
 
 class RegistrationForm(FlaskForm):
-    FirstName = StringField(validators=[InputRequired(), Length(min=2, max=200)],
-                            render_kw={"placeholder": "First Name"})
-    LastName = StringField(validators=[InputRequired(), Length(min=2, max=200)],
-                           render_kw={"placeholder": "Last Name"})
-    Email = StringField(validators=[InputRequired(), Email(), Length(min=4, max=200)],
-                        render_kw={"placeholder": "Email"})
-    Password = PasswordField(validators=[InputRequired(), Length(min=4, max=200)],
-                             render_kw={"placeholder": "Password"})
+    FirstName = StringField(validators=[InputRequired(), Length(min=1, max=200)], render_kw={"placeholder": "First Name"})
+    LastName = StringField(validators=[InputRequired(), Length(min=1, max=200)], render_kw={"placeholder": "Last Name"})
+    Email = StringField(validators=[InputRequired(), Length(min=1, max=200), Email("This field requires a valid email address")], render_kw={"placeholder": "Email"})
+    Password = PasswordField(validators=[InputRequired(), Length(min=1, max=200)], render_kw={"placeholder": "Password"})
 
 class ResetRequestForm(FlaskForm):
-    Email = StringField(validators=[InputRequired(), Email(), Length(min=4, max=200)],
-                        render_kw={"placeholder": "Email"})
+    Email = StringField(validators=[InputRequired(), Length(min=1, max=200), Email("Please enter valid email address.")], render_kw={"placeholder": "Email"})
+    UserToken = StringField(render_kw={"placeholder": "UserToken"})
+    Password = PasswordField(render_kw={"placeholder": "Password"})
+    ConfirmPassword = PasswordField(render_kw={"placeholder": "ConfirmPassword"})
+
+class ChangePasswordForm(FlaskForm):
+    Email = StringField(validators=[InputRequired(), Length(min=1, max=200), Email("Please enter valid email address.")], render_kw={"placeholder": "Email"})
+    Password = PasswordField(validators=[InputRequired(), Length(min=1, max=200)], render_kw={"placeholder": "Password"})
+    ConfirmPassword = PasswordField(validators=[InputRequired(), Length(min=1, max=200)], render_kw={"placeholder": "ConfirmPassword"})
+    UserToken = StringField(validators=[InputRequired()], render_kw={"placeholder": "UserToken"})
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import check_password_hash, generate_password_hash
+import uuid
+from flask_mail import Message
 
-from app.forms.user import LoginForm, RegistrationForm, ResetRequestForm
+from app.forms.user import LoginForm, RegistrationForm, ResetRequestForm, ChangePasswordForm
 from app.models.user import User
-from app import db
+from app import db, mail
 
 # =============================================================================
 # TEMPORARY USER AUTHENTICATION MODULE - TO BE REPLACED
@@ -64,17 +66,78 @@ def resetrequest():
     if form.validate_on_submit():
         user = User.query.filter_by(user_email=form.Email.data).first()
         if user:
-            flash("Reset link would be sent to your email (not implemented).", "success")
-            return redirect(url_for("user.login"))
+            x = uuid.uuid1()
+            uid = str(uuid.uuid5(x, form.Email.data))
+            _sendEmail = sendemail(user.id, user.user_email, uid)
+            if _sendEmail == '1':
+                flash("Thank you for providing your email address. We'll send you a verification code shortly.", "success")
+                return render_template('user/changepassword.html', form=ChangePasswordForm())
         else:
             flash("Email not found.", "error")
     return render_template("user/resetrequest.html", form=form)  # Updated template path
 
 @user.route("/changepassword", methods=["GET", "POST"])
-@login_required
 def changepassword():
-    # Add password change functionality here when needed
-    return render_template("user/changepassword.html")  # Updated template path
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(user_email=form.Email.data).first()
+        if user and user.user_token == form.UserToken.data:
+            if form.Password.data == form.ConfirmPassword.data:
+                hashed_password = generate_password_hash(form.Password.data)
+                user.user_pswd = hashed_password
+                user.user_token = None
+                db.session.commit()
+                flash("Password has been reset successfully.", "success")
+                return redirect(url_for('user.login'))
+            else:
+                flash("Passwords do not match.", "error")
+        else:
+            flash("Invalid Token, please re-enter.", "error")
+    return render_template("user/changepassword.html", form=form)  # Updated template path
+
+@user.route("/update", methods=["POST"])
+@login_required
+def update():
+    try:
+        my_data = User.query.get(request.form.get('id'))
+        my_data.user_fName = request.form['firstname']
+        my_data.user_lName = request.form['lastname']
+        my_data.user_email = request.form['email']
+        db.session.commit()
+        flash("User updated successfully", "success")
+        return redirect(url_for('user.account'))
+    except Exception as e:
+        flash(str(e), "error")
+        return redirect(url_for('user.account'))
+
+@user.route("/delete/<id>/", methods=["GET", "POST"])
+@login_required
+def delete(id):
+    try:
+        my_data = User.query.get(id)
+        db.session.delete(my_data)
+        db.session.commit()
+        flash("User deleted successfully", "success")
+        return redirect(url_for('user.account'))
+    except Exception as e:
+        flash(str(e), "error")
+        return redirect(url_for('user.account'))
+
+@user.route("/email/<emailID>/", methods=["GET", "POST"])
+def email(emailID):
+    try:
+        message = Message(
+            subject='Hello',
+            sender='pythonuserflask@gmail.com',
+            recipients=[emailID]
+        )
+        message.body = "This is a test to " + emailID
+        mail.send(message)
+        flash("Email sent successfully.", 'success')
+        return redirect(url_for('user.account'))
+    except Exception as e:
+        flash(str(e), "error")
+        return redirect(url_for('user.account'))
 
 @user.route("/logout")
 @login_required
@@ -87,3 +150,22 @@ def logout():
 @user.route("/")
 def index():
     return redirect(url_for('user.login'))
+
+def sendemail(userid, email, uid):
+    try:
+        user = User.query.filter_by(id=userid).first()
+        message = Message(
+            subject='Password Reset Token',
+            sender='pythonuserflask@gmail.com',
+            recipients=[email]
+        )
+        message.body = "<table cellpadding='0' cellspacing='0' width='100%' bgcolor='#fafafa' style='background-color: #fafafa; border-radius: 10px; border-collapse: separate;font-size:18px; color:grey; font-family:calibri'><tbody class='ui-droppable'><tr class='ui-draggable'><td align='left' class='esd-block-text es-p20 esd-frame esd-hover esd-draggable esd-block esdev-enable-select' esd-handler-name='textElementHandler'><div class='esd-block-btn esd-no-block-library'><div class='esd-more'><a><span class='es-icon-dot-3'></span></a></div><div class='esd-move ui-draggable-handle' title='Move'><a><span class='es-icon-move'></span></a></div><div class='esd-copy ui-draggable-handle' title='Copy'><a><span class='es-icon-copy'></span></a></div><div class='esd-delete' title='Delete'><a><span class='es-icon-delete'></span></a></div></div><h3>Welcome &nbsp;" + user.user_fName + " " + user.user_lName +",</h3><p><br></p><p style=''>You're receiving this message because you recently reset your password&nbsp;for a account.<br><br>Please copy the below token and confirm your email address for resetting your password. This step adds extra security to your business by verifying the token and email.</p>    <br></td></tr><tr><td>This is your password reset token:<br></td></tr><tr><td><b>"+ uid + "</b></td></tr></tbody></table>"
+        message.html = message.body
+        mail.send(message)
+
+        user.user_token = uid
+        db.session.commit()
+        return '1'
+    except Exception as e:
+        flash(str(e), "error")
+        return '0'
